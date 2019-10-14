404135fce750a0315af25cda38b26ae6a6a295f5
Kimaru thagana <macbuntu@localhost.localdomain>
2019-10-14 09:03:17 +0300

    add code comments

--- mock.py
+++ mock.py
@@ -8,8 +8,10 @@ folder in an s3 bucket
 def download_json_files(bucket, prefix, local_dir):
     bucket = boto3.resource("s3").Bucket(bucket) # initialize resource
     objects = bucket.objects.filter(Prefix=prefix)
-    keys = [obj.key for obj in objects if obj.key.endswith(".json")]
-    local_paths = [os.path.join(local_dir, key) for key in keys]
+    print(objects)
+    keys = [obj.key for obj in objects if obj.key.endswith(".json")] # retrieve any item that has the .json extension
+    print(keys)
+    local_paths = [os.path.join(local_dir, key) for key in keys] # make paths to relevant files on local environment
     for key, local_path in zip(keys, local_paths):
-        os.makedirs(os.path.dirname(local_path), exist_ok=True)
-        bucket.download_file(key, local_path)
+        os.makedirs(os.path.dirname(local_path), exist_ok=True) # make directories
+        bucket.download_file(key, local_path) # download file into the local directory
