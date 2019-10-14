811c8c0074e4fd532989c1c46a7315655635daaf
Kimaru thagana <macbuntu@localhost.localdomain>
2019-10-14 09:10:08 +0300

    run sample code

--- mock.py
+++ mock.py
@@ -4,7 +4,9 @@ import json, os, boto3
 Objective. A function that downloads all the .json files uploaded to a specific
 folder in an s3 bucket
 '''
-
+MY_BUCKET = 'mock_bucket'
+MY_PREFIX = 's3_mock_folder'
+MY_LOCAL_DIR = 'local_dir'
 def download_json_files(bucket, prefix, local_dir):
     bucket = boto3.resource("s3").Bucket(bucket) # initialize resource
     objects = bucket.objects.filter(Prefix=prefix)
@@ -15,3 +17,5 @@ def download_json_files(bucket, prefix, local_dir):
     for key, local_path in zip(keys, local_paths):
         os.makedirs(os.path.dirname(local_path), exist_ok=True) # make directories
         bucket.download_file(key, local_path) # download file into the local directory
+
+download_json_files(MY_BUCKET, MY_PREFIX, MY_LOCAL_DIR)
